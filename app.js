7ec7c2975073ea3e5d05dede47c59a0797f50212
mod-kaif07 <mohammadkaif6660@gmail.com>
2025-08-26 21:53:50 +0530

    validators_adding

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const listing_dat = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
 
 let port = 8080;
 
@@ -25,65 +27,89 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
- res.render("listings/home")
+  res.render("listings/home");
 });
 
-app.get("/listing", async (req, res) => {
-  const allListing = await listing_dat.find({});
-  res.render("listings/index", { allListing });
-});
+app.get(
+  "/listing",
+  wrapAsync(async (req, res) => {
+    const allListing = await listing_dat.find({});
+    res.render("listings/index", { allListing });
+  })
+);
 
-//Add New Route
+//Add New listing
 app.get("/listing/addnew", (req, res) => {
   res.render("listings/new");
 });
 
 //Show Route
-app.get("/listing/:id", async (req, res) => {
-  const { id } = req.params;
-  const showdata = await listing_dat.findById(id);
-  res.render("listings/show", { showdata });
-});
+app.get(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const showdata = await listing_dat.findById(id);
+     if (!showdata) {
+      return next(new ExpressError(404, "Listing not found!"));
+    }
+    res.render("listings/show", { showdata });
+  })
+);
 
 // Create Route
-app.post("/listing", async (req, res) => {
-  const newlistingdata = new listing_dat(req.body.listing);
-  newlistingdata
-    .save()
-    .then(() => {
-      console.log("Data sucessfully Added");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  res.redirect("/listing");
-});
+app.post(
+  "/listing",
+  wrapAsync(async (req, res, next) => {
+    const newlistingdata = new listing_dat(req.body.listing);
+    newlistingdata.save();
+    res.redirect("/listing");
+  })
+);
 
 // edit Route
-app.get("/listing/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const editable_data = await listing_dat.findById(id);
-  res.render("listings/edit", { editable_data });
-});
+app.get(
+  "/listing/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const editable_data = await listing_dat.findById(id);
+    res.render("listings/edit", { editable_data });
+  })
+);
 
 //update Route
-app.put("/listing/:id", async (req, res) => {
-  let { id } = req.params;
-  let updatelisting = req.body.listing; // contains form data
-  const finddeatils = await listing_dat.findByIdAndUpdate(id, updatelisting, {
-    new: true,
-    runValidators: true,
-  });
-  res.redirect(`/listing/${id}`);
-});
+app.put(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let updatelisting = req.body.listing; // contains form data
+    const finddeatils = await listing_dat.findByIdAndUpdate(id, updatelisting, {
+      new: true,
+      runValidators: true,
+    });
+    res.redirect(`/listing/${id}`);
+  })
+);
 
 //Delete route
-app.delete("/listing/:id", async (req, res) => {
-  let { id } = req.params;
-  const find_id_delete = await listing_dat.findByIdAndDelete(id);
-  res.redirect("/listing");
+app.delete(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const find_id_delete = await listing_dat.findByIdAndDelete(id);
+    res.redirect("/listing");
+  })
+);
+// app.all("*", (req, res, next) => {
+//   next(new ExpressError(404, "Page Not Found !!"));
+// });
+
+// Error handling middleware (MUST be last)
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(port, (req, res) => {
   console.log(`App is listern at Port ${port}`);
 });
+
